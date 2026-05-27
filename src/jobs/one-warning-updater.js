8c48c88b2d61e205242ebfb673a857596535fdc3
ryanleung <ryanleung@gitserver.com>
2026-05-27 16:02:04 +0800

    Log "Warning" Source Data For Debugging

--- src/jobs/one-warning-updater.js
+++ src/jobs/one-warning-updater.js
@@ -221,6 +221,7 @@ export async function runOneWarningUpdater() {
 
     if (sendWarning) {
       logger.info(`Warning - save warnings count: ${newWarnings.length}`);
+      logger.info(`Content: ${JSON.stringify(json)}`);
       await setWarnings(newWarnings);
     }
   } catch (e) {
